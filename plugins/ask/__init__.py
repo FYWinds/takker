5ace75770b92e42b48c2614769cbdd7b4b233de2
风屿 <74457127+FYWinds@users.noreply.github.com>
2021-08-21 17:48:28 +0800

    fix few bugs

--- plugins/ask/__init__.py
+++ plugins/ask/__init__.py
@@ -1,7 +1,7 @@
 """
 Author: FYWindIsland
 Date: 2021-08-14 19:36:09
-LastEditTime: 2021-08-20 15:02:25
+LastEditTime: 2021-08-21 17:48:21
 LastEditors: FYWindIsland
 Description: 
 I'm writing SHIT codes
@@ -27,18 +27,26 @@ ask = on_startswith("问", priority=20)
 @ask.handle()
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     message = event.get_plaintext().strip()[1:]
+    handled = False
     if re.findall("几|多少", message):
+        handled = True
         message = await how_many(message)
     if re.findall("什么时候|啥时候", message):
+        handled = True
         message = await what_time(message)
     if re.findall("多久|多长时间", message):
+        handled = True
         message = await how_long(message)
     if re.findall(".不.", message):
+        handled = True
         message = await hif(message)
 
     message = await handle_pers(message)
 
     if re.findall("谁", message):
+        handled = True
         message = await who(message, event.group_id)
-
-    await ask.finish(message)
+    if handled:
+        await ask.finish(message)
+    else:
+        await ask.finish()
