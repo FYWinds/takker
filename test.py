65d541e51f402905e4d40909de86210f143f8818
风屿 <74457127+FYWinds@users.noreply.github.com>
2021-08-21 17:23:48 +0800

    fix xp

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """
 Author: FYWindIsland
 Date: 2021-08-16 22:22:53
-LastEditTime: 2021-08-21 14:49:56
+LastEditTime: 2021-08-21 17:18:53
 LastEditors: FYWindIsland
 Description: 
 I'm writing SHIT codes
@@ -9,11 +9,8 @@ I'm writing SHIT codes
 import os
 import re
 import random
-from typing import List
 from configs.path_config import VOICE_PATH
 
-voices: List[str] = os.listdir(f"{VOICE_PATH}dinggong")
-voice = random.choice(voices)
-text = re.findall("_(.*)_", voice)
+voices: dict[str, int] = {"test": 0, "test1": 5, "test3": 1}
 
-print(text)
+print(dict(sorted(voices.items(), key=lambda item: item[1])))
