b6722e57de96e68bea02ad46b18cf1f205131b47
风屿 <74457127+FYWinds@users.noreply.github.com>
2021-08-22 19:15:07 +0800

    fix stat_hook

--- plugins/stats_log/stat_hook.py
+++ plugins/stats_log/stat_hook.py
@@ -1,7 +1,7 @@
 """
 Author: FYWindIsland
 Date: 2021-08-20 08:47:20
-LastEditTime: 2021-08-22 13:37:32
+LastEditTime: 2021-08-22 19:14:50
 LastEditors: FYWindIsland
 Description: 
 I'm writing SHIT codes
@@ -33,21 +33,25 @@ async def _(matcher: Matcher, e: Exception, bot: Bot, event: Event, state: T_Sta
         return
     if e:
         return
-
-    time_today = str(int(time.time() / 60 / 60 / 24))
-    stats = await query_status(gid=event.group_id)
-    if time_today in stats.keys():
-        module_name = matcher.module_name
-        assert module_name is not None
-        if module_name in HIDDEN_PLUGINS:
-            return
-        if module_name in stats[time_today].keys():
-            stats[time_today][module_name] += 1
+    if (
+        matcher.type == "message"
+        and matcher.priority not in range(0, 11)
+        and matcher.priority not in range(90, 101)
+    ):
+        time_today = str(int(time.time() / 60 / 60 / 24))
+        stats = await query_status(gid=event.group_id)
+        if time_today in stats.keys():
+            module_name = matcher.module_name
+            assert module_name is not None
+            if module_name in HIDDEN_PLUGINS:
+                return
+            if module_name in stats[time_today].keys():
+                stats[time_today][module_name] += 1
+            else:
+                stats[time_today].update({module_name: 1})
         else:
-            stats[time_today].update({module_name: 1})
-    else:
-        module_name = matcher.module_name
-        assert module_name is not None
-        stats.update({time_today: {module_name: 1}})
+            module_name = matcher.module_name
+            assert module_name is not None
+            stats.update({time_today: {module_name: 1}})
 
-    await set_status(gid=event.group_id, stat=stats)
+        await set_status(gid=event.group_id, stat=stats)
