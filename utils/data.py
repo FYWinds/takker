bc3e49fec74a26311e8054bec3b2ed73b5439524
风屿 <74457127+FYWinds@users.noreply.github.com>
2022-08-06 14:52:21 +0800

    Plugin: 答案之书 Data: 答案之书数据 File: utils/init.py

--- utils/data.py
+++ utils/data.py
@@ -4,10 +4,10 @@ from nonebot.log import logger
 
 from configs.path_config import TEXT_PATH
 
-
 fortune: list[dict]
 msg_of_day: list[dict]
 atri_text: list[dict]
+book_of_answers: list[str]
 
 with open(f"{TEXT_PATH}fortune.json", "r", encoding="utf-8") as file:
     fortune = json.load(file)["data"]
@@ -17,3 +17,6 @@ with open(f"{TEXT_PATH}msg_of_day.json", "r", encoding="utf-8") as file:
 
 with open(f"{TEXT_PATH}atri.json", "r", encoding="utf-8") as file:
     atri_text = json.load(file)
+
+with open(f"{TEXT_PATH}book_of_answers") as file:
+    book_of_answers = json.load(file)
